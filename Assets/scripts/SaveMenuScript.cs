d70fe09c80c711bdffe819cbfeeb60509fec355b
jessi68 <lyn990312@gmail.com>
2022-03-27 18:40:26 +0900

    feat: open incident uis when click save button

--- Assets/scripts/SaveMenuScript.cs
+++ Assets/scripts/SaveMenuScript.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SaveMenuScript : MonoBehaviour
 {
+    #region Field
+    public Button[] incidentRecordButtons;
+    bool m_isMenuShown = false;
+    #endregion
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    #region Methods
+    public void onClick()
     {
-        
+        for (int i = 0; i < incidentRecordButtons.Length; i++)
+        {
+            incidentRecordButtons[i].gameObject.SetActive(!m_isMenuShown);
+        }
+        m_isMenuShown = !m_isMenuShown;
     }
+    #endregion
 }
