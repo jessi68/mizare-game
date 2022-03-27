0bfbef5ac0585c9090ca7e5919eef37f79092adb
jessi68 <lyn990312@gmail.com>
2022-03-27 20:30:36 +0900

    fix: fix logic error in save menu cliick listener

--- Assets/scripts/SaveMenuScript.cs
+++ Assets/scripts/SaveMenuScript.cs
@@ -7,7 +7,6 @@ public class SaveMenuScript : MonoBehaviour
 {
     #region Field
     public Button[] incidentRecordButtons;
-    bool m_isMenuShown = false;
     #endregion
     // Start is called before the first frame update
 
@@ -18,9 +17,8 @@ public class SaveMenuScript : MonoBehaviour
         Debug.Log(m_isMenuShown);
         for (int i = 0; i < incidentRecordButtons.Length; i++)
         {
-            incidentRecordButtons[i].gameObject.SetActive(!m_isMenuShown);
+            incidentRecordButtons[i].gameObject.SetActive(!incidentRecordButtons[i].gameObject.activeSelf);
         }
-        m_isMenuShown = !m_isMenuShown;
     }
     #endregion
 }
