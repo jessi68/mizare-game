a07365a824b7cb321d228c0288348c36102764c6
jessi68 <lyn990312@gmail.com>
2022-04-02 19:40:45 +0900

    feat: can move right or left iin photo album

--- Assets/YS/1_Script/PhotoAlbum.cs
+++ Assets/YS/1_Script/PhotoAlbum.cs
@@ -8,7 +8,11 @@ public class PhotoAlbum : MonoBehaviour
     #region Field
     private List<Sprite> sprites;
     public GameObject[] shownPictures;
-    int currentIndex = 0;
+    int currentFirstIndex = 0;
+    int finalIndex = 2;
+
+    public Button leftButton;
+    public Button rightButton;
     #endregion
 
     // Start is called before the first frame update
@@ -20,12 +24,19 @@ public class PhotoAlbum : MonoBehaviour
     {
 
         sprites = new List<Sprite>();
+
+        // dumi photo 넣는 코드
         this.add(Resources.Load<Sprite>("dumiPhotos/one"));
         this.add(Resources.Load<Sprite>("dumiPhotos/two"));
         this.add(Resources.Load<Sprite>("dumiPhotos/three"));
         this.add(Resources.Load<Sprite>("dumiPhotos/four"));
         this.add(Resources.Load<Sprite>("dumiPhotos/five"));
+
+        // 처음에는 비활성화된 상태 
         this.transform.gameObject.SetActive(false);
+
+        leftButton.enabled = false;
+
     }
 
     // Update is called once per frame
@@ -36,6 +47,17 @@ public class PhotoAlbum : MonoBehaviour
     #endregion
 
     #region Methods
+
+    bool isPreiviousPageExist()
+    {
+        return currentFirstIndex >= shownPictures.Length;
+    }
+
+    bool isNextPageExist()
+    {
+        return currentFirstIndex + shownPictures.Length < sprites.Count;
+    }
+
     void add(Sprite sprite)
     {
         if(sprites.Count < shownPictures.Length)
@@ -45,15 +67,52 @@ public class PhotoAlbum : MonoBehaviour
         sprites.Add(sprite);
     }
 
+    private void changeShownPictures()
+    {
+        for (int i = currentFirstIndex; i <= finalIndex; i++)
+        {
+            if(i >= sprites.Count) 
+            {
+                shownPictures[i - currentFirstIndex].GetComponent<Image>().sprite = null;
+                continue;
+            }
+            shownPictures[i - currentFirstIndex].GetComponent<Image>().sprite = sprites[i];
+        }
+    }
     
     public void goToPreivousPage()
     {
-        currentIndex -= shownPictures.Length;
+        finalIndex = currentFirstIndex - 1;
+        currentFirstIndex -= shownPictures.Length;
+        changeShownPictures();
+
+        if (!isPreiviousPageExist())
+        {
+            leftButton.enabled = false;
+        }
+
+        if(isNextPageExist())
+        {
+            rightButton.enabled = true;
+        }
+       
     }
 
     public void goToNextPage()
     {
-        currentIndex += shownPictures.Length;
+        currentFirstIndex += shownPictures.Length;
+        finalIndex = currentFirstIndex + shownPictures.Length - 1;
+        changeShownPictures();
+
+        if(!isNextPageExist())
+        {
+            rightButton.enabled = false;
+        }
+
+        if(isPreiviousPageExist())
+        {
+            leftButton.enabled = true;
+        }
     }
     #endregion
 
