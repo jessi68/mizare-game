2b9915ed81b0606f2a5a0096bc24a155ab2d1cdb
최윤서 <chldbstj4536@gmail.com>
2022-07-24 14:34:41 +0900

    feat: Odin 에셋 추가

--- Assets/YS/1_Script/Data/ScriptData.cs
+++ Assets/YS/1_Script/Data/ScriptData.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace YS
 {
@@ -6,9 +8,18 @@ namespace YS
     public class ScriptData : ScriptableObject
     {
         [SerializeReference]
-        [SerializeReferenceButton]
         private BaseScriptEvent[] scripts;
         private BaseScriptEvent curScript;
+
+#if UNITY_EDITOR
+        [SerializeField, PropertyRange(0, "@scripts.Length - 1")]
+        [LabelText("»ðÀÔÇÒ ÀÎµ¦½º À§Ä¡")]
+        private uint insertIndex;
+        [Button(Name = "½ºÅ©¸³Æ® »ðÀÔ")]
+        private void InsertScript()
+        {
+        }
+#endif
 
         public BaseScriptEvent CurrentScript => curScript;
 
